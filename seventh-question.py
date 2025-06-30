451707eaabab10bd3123b4b34685149501be20a4
léo <javascriptleonardolucas@gmail.com>
2025-06-30 20:49:25 -0300

    improving

--- seventh-question.py
+++ seventh-question.py
@@ -1,14 +1,11 @@
 numbers = []
-
+sum = 0
 while len(numbers) < 5:
     number = int(input("type a number"))
+    if number % 2 == 0:
+        sum += number
     numbers.append(number)
     
 
-sum = 0
-for number in numbers:
-    
-    if number % 2 == 0:
-        sum += number
 
 print("result:", sum)
