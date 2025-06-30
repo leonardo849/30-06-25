d66470c20480738e563ffdc876a84e4048a21da3
léo <javascriptleonardolucas@gmail.com>
2025-06-30 19:51:11 -0300

    forth question is ready

---
+++ fourth-question.py
@@ -0,0 +1,5 @@
+number = int(input("what's up bro. Type a number. I will send its multiplication table"))
+
+for multi in range(1, 11):
+    result = number * multi
+    print(f"{number} X {multi} = {result}")
